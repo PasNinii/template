920e1324a96baa050bd95cac2d341cea88ef1311
PasNinii <nicrannou@gmail.com>
2024-08-16 11:25:29 +0000

    routes

--- projects/template/src/app/app.routes.ts
+++ projects/template/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './feature/home/home.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'home',
+    title: 'Home',
+    loadChildren: () => import('./feature/home/home.routes'),
+  },
   { path: '**', redirectTo: '' },
 ];
