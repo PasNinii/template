4db3c7e5ec9681400d9d821f2b4fa5287e9df63a
Nicolas Rannou <nicolas.rannou@spinergie.com>
2025-01-17 18:41:11 +0100

    add angular material

--- projects/template/src/app/app.config.ts
+++ projects/template/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 
 import { routes } from './app.routes';
 import { provideCore } from './core/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideCore({ routes })],
+  providers: [provideCore({ routes }), provideAnimationsAsync()],
 };
